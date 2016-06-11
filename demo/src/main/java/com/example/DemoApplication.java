5f8addfa46deb94af4cbca5a1ba1afc66eb592e4
Rapange <h_ero@hotmail.es>
2016-06-11 02:01:30 -0500

    Matriculas

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -1,70 +1,18 @@
 package com.example;
 
-import java.util.Collections;
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.orm.jpa.EntityScan;
 
 import com.example.dominio.Alumno;
-import com.example.repositorio.AlumnoRepositorio;
 
 @EnableAutoConfiguration
-@Controller
+@EntityScan(basePackageClasses = Alumno.class)
+@SpringBootApplication
 public class DemoApplication {
-
-	AlumnoRepositorio repositorio = new AlumnoRepositorio();
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-	public int testing(int a){
-		return a+2;
-	}
-
-	@RequestMapping("/alumnos")
-	@ResponseBody
-	public List<Alumno> alumnos(String ordenarPor) {
-		if(ordenarPor.equals("Promedio")){
-			List<Alumno> lista = repositorio.listar();
-			for(int i = 0; i < lista.size(); i++){
-				for(int j = 0; j < lista.size()-i-1;j++){
-					if(lista.get(j).promedio > lista.get(j+1).promedio){
-						Collections.swap(lista, j, j+1);
-					}
-				}
-			}
-			return lista;
-		}
-		if(ordenarPor.equals("Nombre")){
-			List<Alumno> lista = repositorio.listar();
-			for(int i = 0; i < lista.size(); i++){
-				for(int j = 0; j < lista.size()-i-1; j++){
-					if(lista.get(j).nombres.compareTo(lista.get(j+1).nombres) > 0){
-						Collections.swap(lista,j,j+1);
-					}
-				}
-			}
-			return lista;
-		}
-		if(ordenarPor.equals("Apellido Paterno")){
-			List<Alumno> lista = repositorio.listar();
-			for(int i = 0; i < lista.size(); i++){
-				for(int j = 0; j < lista.size()-i-1;j++){
-					if(lista.get(j).apellidoPaterno.compareTo(lista.get(j+1).apellidoPaterno) > 0){
-						Collections.swap(lista,j,j+1);
-					}
-				}
-			}
-			return lista;
-		}
-		else{
-		//List<Alumno> lista2 = repositorio.listar();
-		//lista2.get(0).promedio = lista2.get(1).promedio;
-		return repositorio.listar();
-		}
 	}
 }
--- demo/src/main/java/com/example/repositorio/AlumnoRepositorio.java
+++ demo/src/main/java/com/example/repositorio/AlumnoRepositorio.java
@@ -1,18 +1,11 @@
 package com.example.repositorio;
 
-import java.util.Arrays;
 import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
 
 import com.example.dominio.Alumno;
 
-public class AlumnoRepositorio {
-	Alumno[] data = {
-		new Alumno("Juan", "Perez", 16),
-		new Alumno("Maria", "Castro", 14),
-		new Alumno("Jose", "Ruiz", 18),
-	};
-	
-	public List<Alumno> listar() {
-		return Arrays.asList(data);
-	}
+public interface AlumnoRepositorio extends CrudRepository<Alumno, Integer> {
+	List<Alumno> findAll();
 }
